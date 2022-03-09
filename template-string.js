a5de59cffac43d34df86b4cf1204c9a84c70474e
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-09 14:39:28 +0600

    add template string

---
+++ template-string.js
@@ -0,0 +1,10 @@
+const number = [12, 21, 32, 42, 50];
+const student = {
+    name: 'mehedi hasan',
+    age: 26,
+    job: 'salesman',
+    baby: 'one',
+    home: 'none'
+}
+const aboutMe = `I am md ${student.name} and I am ${student.age} years old and I do ${student.job} job in Barishal and I have ${student.baby} baby`;
+console.log(aboutMe);
